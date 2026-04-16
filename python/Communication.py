f5ff60c63eaa8863a5974db8ab56c3ac1704c619
Lucas <lulutaogamer@gmail.com>
2026-04-16 09:54:16 +0200

    ip

--- python/Communication.py
+++ python/Communication.py
@@ -31,7 +31,7 @@ conn.close()
 
 def InitialisationConnection():
     import socket
-    HOST = "192.168.88.177" #local host
+    HOST = "127.0.0.1" #local host
     PORT = 5000 #port de comunication (le meme dans le cpp)
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
