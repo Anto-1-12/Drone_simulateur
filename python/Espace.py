725b2200b76bcc03d29365c2d79cd0d5961655df
Gapar <gasp.chip@gmail.com>
2026-04-16 10:14:55 +0200

    transform angle

--- python/Espace.py
+++ python/Espace.py
@@ -82,6 +82,19 @@ class Drone:
             [0, 0, 1]
         ])
 
+    def rotation_matrix(rx, ry, rz):
+        return rot_z(rz) @ rot_y(ry) @ rot_x(rx)
+
+    def transform_cube(vertices, center, angles, scale=1.0):
+        # angles = (rx, ry, rz)
+        R = rotation_matrix(*angles)
+        scaled = vertices * scale
+        rotated = scaled @ R.T
+        translated = rotated + center
+        return translated  
+
+
+
 
 class Moteur:
     #N=4?
