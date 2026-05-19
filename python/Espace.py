719366a1198f3f08aef64b75c1db29634a5a1c14
Taylou <lulutaogamer@gmail.com>
2026-05-19 22:29:53 +0200

    np.array

--- python/Espace.py
+++ python/Espace.py
@@ -181,7 +181,7 @@ class Drone:
             self.centregravité[1] = 0 
 
     def poussée(self):
-        self.puissance*self.orientationmoteur
+        self.puissance*np.array(self.orientationmoteur)
         self.Momentum=self.puissance*self.orientationmoteur+self.Momentum
 
     #Ci-dessous : fonction d'angles
