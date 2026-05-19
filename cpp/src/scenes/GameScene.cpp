0a8e3fb972e6a71714320e7f3804f3d1cab2512e
Evan <anto1.12.tld@gmail.com>
2026-05-19 21:08:13 +0200

    rotation drone reception

--- cpp/src/scenes/GameScene.cpp
+++ cpp/src/scenes/GameScene.cpp
@@ -286,9 +286,8 @@ void GameScene::sync(bool debug)
                 }
                 else if (cmd == "sync_drone_ang")
                 {
-                    drone.rotation.x = j["x"];
-                    drone.rotation.y = j["y"];
-                    drone.rotation.z = j["z"];
+                    drone.rotation.x = glm::degrees(std::atan2(double (j["z"]), double (j["x"])));
+                    drone.rotation.y = glm::degrees(std::asin(double (j["y"])));
                     if(debug)
                     {
                         std::cout<<j<<std::endl;
