bfcb978596c0f15ab765fe236ef3ea4af1aea73d
Taylou <lulutaogamer@gmail.com>
2026-05-20 02:05:03 +0200

    nv matrice rota

--- python/Espace.py
+++ python/Espace.py
@@ -207,9 +207,11 @@ class Drone:
             self.centregravité[1] = 0 
 
     def poussée(self):
+        v = [np.cos(self.orientationdegrés[0]) * np.cos(self.orientationdegrés[1]), np.sin(self.orientationdegrés[0]), np.cos(self.orientationdegrés[0]) * np.sin(self.orientationdegrés[1])]
+        v =v.tolist()
         matricerotation=self.rotation_matrix(self.orientationdegrés[0],self.orientationdegrés[1],self.orientationdegrés[2])
         self.v_initial=[0,1,0]
-        v_direction=matricerotation @ self.v_initial
+        v_direction=v @ self.v_initial
         v_direction=v_direction.tolist()
         print("v_direction")
         print(v_direction)
